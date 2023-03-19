c5f296a85f43627490c9a06613025d68f232841a
Alexey Strelkov <cpr.al.exe@gmail.com>
2023-03-19 02:11:24 +0000

    showing currently pressed keys

--- src/midi.js
+++ src/midi.js
@@ -8,8 +8,8 @@ let deviceElement;
 let chordToPlayElement;
 let heldNotesElement;
 
-let currentlyHeldNotes;
-let currentlyHeldKeys;
+let currentlyHeldNotes = new Set();
+let currentlyHeldKeys = new Set();;
 
 // Function triggered when WEBMIDI.js is ready
 function onEnabled() {
@@ -31,11 +31,42 @@ function onEnabled() {
     // const mySynth = WebMidi.getInputByName("TYPE NAME HERE!")
 
     //subscribe to first channel of first device
-    mySynth.channels[1].addListener("noteon", e => {
-        document.body.innerHTML+= `${e.note.name} ${e.note.identifier}<br>`;
-        console.log(e.note);
-    });
+    // mySynth.channels[1].addListener("noteon", e => {
+    //     document.body.innerHTML+= `${e.note.name} ${e.note.identifier}<br>`;
+    //     console.log(e.note);
+    // });
+
+    mySynth.channels[1].addListener("noteon", NoteOnHandler)
+    mySynth.channels[1].addListener("noteoff", NoteOffHandler)
 
     chordToPlayElement.innerHTML = getNextRandomChord()
 
+}
+
+function NoteOnHandler(e)
+{
+    console.log(e)
+    currentlyHeldNotes.add(e.note.identifier)
+    currentlyHeldKeys.add(e.note.number)
+    redrawHeldNotes()
+}
+
+function NoteOffHandler(e)
+{
+    console.log(e)
+    currentlyHeldNotes.delete(e.note.identifier)
+    currentlyHeldKeys.delete(e.note.number)
+    redrawHeldNotes()
+}
+
+function redrawHeldNotes()
+{
+    let heldNotesText = ""
+    // console.log(currentlyHeldNotes)
+    currentlyHeldNotes.forEach((note, index)=>{
+
+        heldNotesText +=note + " "
+    })
+
+    heldNotesElement.innerHTML=heldNotesText
 }
